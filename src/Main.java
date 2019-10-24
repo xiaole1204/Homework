7f8304467b53417b935f15836380b08027b920c3
xiaole1204 <zxc25773313@gmail.com>
2019-10-24 21:10:36 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//n的乘階
+//import java.util.Scanner;
+//public class Main{
+//    public static void main(String[] args) {
+//        Scanner scn = new Scanner(System.in);
+//        int h = scn.nextInt();
+//        int a = 1;
+//        for (int i = 1 ; i <= h;i++){
+//            a *= i ;
+//            System.out.printf("%d\n",a);
+//        }
+//        System.out.printf("%d\n",h,a);
+//    }
+//}
+import java.util.Scanner;
