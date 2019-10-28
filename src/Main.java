81a2725b9f192e79569451c5eb557f15a371a4db
xiaole1204 <zxc25773313@gmail.com>
2019-10-28 21:01:37 +0800

    homework

--- src/Main.java
+++ src/Main.java
@@ -12,18 +12,18 @@
 //    }
 //}
 //1/[(2n-1)*(2n)]總和
-import java.util.Scanner;
-public class Main{
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        double sum = 0;
-        for (int i = 1;i <=n; i++) {
-            sum = sum + 1.0 /((2*i-1)*2*i);
-        }
-        System.out.println(sum);
-    }
-}
+//import java.util.Scanner;
+//public class Main{
+//    public static void main(String[] args) {
+//        Scanner scn = new Scanner(System.in);
+//        int n = scn.nextInt();
+//        double sum = 0;
+//        for (int i = 1;i <=n; i++) {
+//            sum = sum + 1.0 /((2*i-1)*2*i);
+//        }
+//        System.out.println(sum);
+//    }
+//}
 //正因數
 //import java.util.Scanner;
 //public class Main{
