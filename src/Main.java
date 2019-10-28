6cda839ae0cfade3b2b0cd64a2c9fc3016a57204
xiaole1204 <zxc25773313@gmail.com>
2019-10-28 20:47:43 +0800

    homework

--- src/Main.java
+++ src/Main.java
@@ -38,19 +38,20 @@
 //    }
 //}
 //實心正方形
-//import java.util.Scanner;
-//public class Main {
-//    public static void main(String[] args) {
-//        Scanner scn = new Scanner(System.in);
-//        int n = scn.nextInt();
-//        char c = scn.next().charAt(0);
-//        for (int i=1;i<=n;i++){
-//            for (int j=1;j<=n;j++){
-//                System.out.print(c);
-//            }
-//        }
-//    }
-//}
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        char c = scn.next().charAt(0);
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=n;j++){
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+}
 //計算2n總和
 //import java.util.Scanner;
 //public class Main{
